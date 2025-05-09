530a206441e3f2a5bd6d3dacefb847bca236c4b3
Irsa Ayaz <irsaayaz.525@gmail.com>
2025-05-09 22:26:20 +0500

    Add files via upload

---
+++ data_types_practice.py
@@ -0,0 +1,23 @@
+#Integer Examples
+grade = 7
+score = 99
+print("Grade:", grade)
+print("Score:", score)
+
+#String Examples
+name = "Irsa Ayaz"
+message = "Learning Python is really fun!"
+print("Name:", name)
+print("Message:", message)
+
+#Float Examples
+milkQuantity = 20.55 #in Litters
+price = 10.99 #in Rupees
+print("MilkQuantity:", milkQuantity)
+print("Price:", price)
+
+#Boolean Examples
+is_hungry = True
+not_hungry = False
+print("Is Hungry:", is_hungry)
+print("Not Hungry:", not_hungry)
